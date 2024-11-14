bab670789d413ef2543d7fc736b39089f8464edb
Ecophagy <oghill@gmail.com>
2024-11-14 15:33:53 +0000

    Start a new auction when auctioneer receives a new task

--- include/auctioneer.hpp
+++ include/auctioneer.hpp
@@ -1,5 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "task_auction/msg/task.hpp"
+#include "task_auction/msg/bid.hpp"
+#include <map>
+#include <vector>
 
 class Auctioneer : public rclcpp::Node
 {
@@ -7,6 +10,10 @@ public:
     Auctioneer();
 
 private:
+    std::map<int, std::vector<task_auction::msg::Bid>> auctions;
+
+    void NewTaskCallback(std::unique_ptr<task_auction::msg::Task, std::default_delete<task_auction::msg::Task>> msg);
+
     rclcpp::Subscription<task_auction::msg::Task>::SharedPtr newTaskSubscriber;
     rclcpp::Publisher<task_auction::msg::Task>::SharedPtr auctionPublisher;
 };
--- src/auctioneer.cpp
+++ src/auctioneer.cpp
@@ -1,13 +1,21 @@
 #include "auctioneer.hpp"
 #include "constants.hpp"
+#include <functional>
 
 Auctioneer::Auctioneer() : Node("auctioneer")
 {
     auctionPublisher = this->create_publisher<task_auction::msg::Task>(Topics::auctions, 10);
 
-    auto newTaskCallback =
-      [this](task_auction::msg::Task::UniquePtr msg) -> void {
-        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->description.c_str());
-      };
-    newTaskSubscriber = this->create_subscription<task_auction::msg::Task>(Topics::newTask, 10, newTaskCallback);
+    newTaskSubscriber = this->create_subscription<task_auction::msg::Task>(Topics::newTask, 10, std::bind(&Auctioneer::NewTaskCallback, this, std::placeholders::_1));
+}
+
+
+void Auctioneer::NewTaskCallback(std::unique_ptr<task_auction::msg::Task, std::default_delete<task_auction::msg::Task>> msg)
+{
+    // New task received, so start a new auction
+    auctions[msg->id] = {};
+    auto newTaskMsg = task_auction::msg::Task();
+    newTaskMsg.id = msg->id;
+    newTaskMsg.description = msg->description;
+    auctionPublisher->publish(newTaskMsg);
 }
