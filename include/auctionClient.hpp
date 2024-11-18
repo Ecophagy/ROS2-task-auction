749b9e50331962ff863f7c6216b31c1e1ec9ae83
Ecophagy <oghill@gmail.com>
2024-11-18 15:07:20 +0000

    Generate bid randomly between 1 and 10
    As a placeholder for actual cost and bid calculation

--- include/auctionClient.hpp
+++ include/auctionClient.hpp
@@ -1,6 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "task_auction/msg/task.hpp"
 #include "task_auction/msg/bid.hpp"
+#include <random>
 
 class AuctionClient : public rclcpp::Node
 {
@@ -9,6 +10,8 @@ public:
 
 private:
     int robotId;
+    std::mt19937 generator;
+    std::uniform_int_distribution<unsigned int> distribute;
     rclcpp::Subscription<task_auction::msg::Task>::SharedPtr auctionSubscriber;
     rclcpp::Publisher<task_auction::msg::Bid>::SharedPtr bidPublisher;
     void AuctionCallback(std::unique_ptr<task_auction::msg::Task, std::default_delete<task_auction::msg::Task>> msg);
--- src/auctionClient.cpp
+++ src/auctionClient.cpp
@@ -2,9 +2,13 @@
 #include "constants.hpp"
 #include <functional>
 
-
 AuctionClient::AuctionClient() : Node ("auctionClient")
 {
+    std::random_device os_seed;
+    const auto seed = os_seed();
+    generator = std::mt19937(seed);
+    distribute = std::uniform_int_distribution<unsigned int>(1,10);
+
     this->declare_parameter(Parameters::robotId, 0);
     robotId = this->get_parameter(Parameters::robotId).as_int();
     bidPublisher = this->create_publisher<task_auction::msg::Bid>(Topics::auctionBids, 1);
@@ -16,8 +20,7 @@ void AuctionClient::AuctionCallback(std::unique_ptr<task_auction::msg::Task, std
     auto bid = task_auction::msg::Bid();
     bid.task_id = msg->id;
     bid.robot_id = robotId;
-    // TODO: Caluculate and assign bid value
-    bid.bid_value = 0;
+    bid.bid_value = distribute(generator); // Bid value is random value uniformly distributed between 1 and 10.
     RCLCPP_INFO(this->get_logger(), "Submitting bid for task %ld of %ld", msg->id, bid.bid_value);
     bidPublisher->publish(bid);
 }
